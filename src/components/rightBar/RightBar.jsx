03089a1e4811d62b20f2c13b77fe234abff61108
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-06-03 01:33:14 +0700

    Revert "Revert "Responsive UI""

---
+++ src/components/rightBar/RightBar.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+import "./rightBar.scss";
+
+const RightBar = () => {
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Sơn Thươl</span>
+            </div>
+            <div className="buttons">
+              <button>flolow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Sơn Thươl</span>
+            </div>
+            <div className="buttons">
+              <button>flolow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Sơn Thươl</span>
+            </div>
+            <div className="buttons">
+              <button>flolow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <span>Sơn Thươl</span>
+            </div>
+            <div className="buttons">
+              <button>flolow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span className="name">Sơn Thươl</span>
+                <span className="activiti">changed their conver picture</span>
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span className="name">Sơn Thươl</span>
+                <span className="activiti">changed their conver picture</span>
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span className="name">Sơn Thươl</span>
+                <span className="activiti">changed their conver picture</span>
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span className="name">Sơn Thươl</span>
+                <span className="activiti">changed their conver picture</span>
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span className="name">Sơn Thươl</span>
+                <span className="activiti">changed their conver picture</span>
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <p>
+                <span className="name">Sơn Thươl</span>
+                <span className="activiti">changed their conver picture</span>
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Sơn Thươl</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Sơn Thươl</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Sơn Thươl</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Sơn Thươl</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/3454298/pexels-photo-3454298.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt=""
+              />
+              <div className="online" />
+              <span>Sơn Thươl</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
