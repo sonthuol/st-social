3640351c3e0ff909ae8b205d582db247b0b86230
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-06-03 01:27:08 +0700

    Revert "Responsive UI"

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { DarkModeContextProvider } from "./context/darkModeContext.jsx";
-import { AuthContextProvider } from "./context/authContext.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <DarkModeContextProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </DarkModeContextProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
