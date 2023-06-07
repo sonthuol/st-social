97b679572b10429518f1e78015e9867ae997b7be
Mark Son <63505302+sonthuol@users.noreply.github.com>
2023-06-07 22:23:56 +0700

    setting profile

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,3 +1,11 @@
+import { db } from "../connect.js";
+
 export const getUser = (req, res) => {
-  // TODO
+  const userId = req.params.userId;
+  const q = "SELECT * from users WHERE id = ?";
+  db.query(q, [userId], (err, data) => {
+    if (err) return res.status(200).json(err);
+    const { password, ...info } = data[0];
+    return res.status(200).json(info);
+  });
 };
